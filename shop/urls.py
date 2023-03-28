5dd30c8668e75cf9440fd17942239fed4e8dbf82
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2023-03-29 01:08:16 +0330

    linked templates
    made a url and view for each template and linked them to each other

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', shop_index, name='index'),
+    path('shop/', shop_shop, name='shop'),
+    path('detail/', shop_detail, name='detail'),
+    path('contact/', shop_contact, name='contact'),
+    path('cart/', shop_cart, name='cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def shop_index(request):
     return render(request, 'shop/index.html')
+
+def shop_shop(request):
+    return render(request, 'shop/shop.html')
+
+def shop_detail(request):
+    return render(request, 'shop/detail.html')
+
+def shop_contact(request):
+    return render(request, 'shop/contact.html')
+
+def shop_cart(request):
+    return render(request, 'shop/cart.html')
